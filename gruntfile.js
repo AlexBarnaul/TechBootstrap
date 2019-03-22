310b41a08daeae430f75b35f65cb7e1527c65660
AlexBarnaul <bigmishko@mail.ru>
2019-03-22 13:43:57 +0700

    added sum sections

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+		//SASS
+		sass: {
+			dist: {
+				files: {
+					'css/style.css': 'sass/style.scss'
+				}
+			}
+		},
+
+		//		WATCH
+		watch: {
+			styles: {
+				files: ['sass/**/*.scss', "css/*.css"],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				},
+			},
+		},
+		//		browserSync
+		browserSync: {
+			bsFiles: {
+				src: ['css/*.css', "**/*.*"]
+			},
+			options: {
+				watchtask: false,
+				server: {
+					baseDir: "./"
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
+
+};
